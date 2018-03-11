0e78aa5316bad00f5744a89b1c3d4871f35780a5
Srijan Sinha <srijansinha159@gmail.com>
2018-03-12 01:30:16 +0530

    Updated 2D elements structure

--- main.cpp
+++ main.cpp
@@ -1,19 +1,5 @@
-#ifndef GLUT_H
-#define GLUT_H
-#include <GL/glut.h>
-#endif
-
-#ifndef IO_H
-#define IO_H
-#include <iostream>
-#endif
-
-#include "input2d.h"
-#include "input3d.h"
-#include "output3d.h"
-#include "output2d.h"
-
+#include "3dto2d.h"
 int main()
 {
-
-};
+	return 0;
+}
---
+++ main1.cpp
@@ -0,0 +1,19 @@
+#ifndef GLUT_H
+#define GLUT_H
+#include <GL/glut.h>
+#endif
+
+#ifndef IO_H
+#define IO_H
+#include <iostream>
+#endif
+
+#include "input2d.h"
+#include "input3d.h"
+#include "output3d.h"
+#include "output2d.h"
+
+int main()
+{
+
+};
