22d655483c96a5f7f8cbff0cec0e49d34db71c8a
Srijan Sinha <srijansinha159@gmail.com>
2018-03-18 16:07:56 +0530

    Rendering code added.

---
+++ myarea.cpp
@@ -0,0 +1,45 @@
+#include "myarea.h"
+#include <cairomm/context.h>
+#include "elements2d.h"
+
+MyArea::MyArea()
+{
+}
+
+MyArea::~MyArea()
+{
+}
+
+bool MyArea::on_draw(object2d obj, const Cairo::RefPtr<Cairo::Context>& cr)
+{
+  Gtk::Allocation allocation = get_allocation();
+  const int width = allocation.get_width();
+  const int height = allocation.get_height();
+
+  // coordinates for the center of the window
+  double xc, yc;
+  xc = width / 2;
+  yc = height / 2;
+  std::valarray< double > dashes(2);
+  dashes[0] = 6.0;
+  dashes[1] = 3.0;
+
+  cr->set_line_width(3.0);
+  Edge2d e;
+  // draw red lines out from the center of the window
+  for (int i = 0; i < obj.edges.size(); i++)
+  {
+    e = obj.edges[i];
+    if(e.hidden)
+      dashes[1] = 3.0;
+    else
+      dashes[1] = 0;
+    cr->set_source_rgb(0.0, 0.0, 0.0);
+    cr->set_dash(dashes, 0.0);
+    cr->move_to(xc + e.v1.x, yc - e.v1.y);
+    cr->line_to(xc + e.v2.x, yc - e.v2.y);
+  }
+  cr->stroke();
+
+  return true;
+}
