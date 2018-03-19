518d9dd586675e0c200436ee8863efa785c91106
Srijan Sinha <srijansinha159@gmail.com>
2018-03-19 16:19:43 +0530

    Interface header added

--- display.h
+++ display.h
@@ -1,3 +1,24 @@
-direction data of the object
-2d object rendering all orthographic views if possible
-3d object refresh rate rendering
+// direction data of the object
+// 2d object rendering all orthographic views if possible
+// 3d object refresh rate rendering
+
+#ifndef GTKMM_EXAMPLE_MYAREA_H
+#define GTKMM_EXAMPLE_MYAREA_H
+#include "backend_header.h"
+#include <gtkmm/drawingarea.h>
+
+class MyArea : public Gtk::DrawingArea
+{
+public:
+	object2d obj;
+  
+  MyArea();
+  virtual ~MyArea();
+
+protected:
+  //Override default signal handler:
+  bool on_draw(const Cairo::RefPtr<Cairo::Context>& cr) override;
+};
+
+#endif // GTKMM_EXAMPLE_MYAREA_H
+
